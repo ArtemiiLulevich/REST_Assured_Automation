9713759ffe6688ff4b807067cdbe6ec5e3d4e373
Artemii <artemii.lulevich@gmail.com>
2022-09-26 22:16:30 +0300

    Put request

--- src/test/java/POST_PUT_Delete/PostPutDeleteTest.java
+++ src/test/java/POST_PUT_Delete/PostPutDeleteTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostPutDeleteTest {
 
@@ -50,6 +51,31 @@ public class PostPutDeleteTest {
                 .post("/user/create")
                 .then()
                 .statusCode(200)
+                .extract().response();
+
+        System.out.println(res.asString());
+    }
+
+    @Test
+    public void put_Json_Object_request() {
+
+        String id = "632c0b360dc48a215267ce7c";
+
+        JSONObject body = new JSONObject();
+
+        body.put("firstName", "Vasia");
+        body.put("lastName", "zhopin");
+
+        Response res = given()
+                .baseUri(baseURI)
+                .header("app-id", "6322d04b1f1e5736304063fd")
+                .contentType(ContentType.JSON)
+                .body(body.toString())
+                .when()
+                .put("/user/" + id)
+                .then()
+                .statusCode(200)
+                .body("lastName", equalTo("zhopin"))
                 .extract().response();
 
         System.out.println(res.asString());
